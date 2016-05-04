7a2ffb400f85f377347335d224f0e4b0348dcd6c
Eugene Min <e.min@milax.com>
2016-05-04 23:02:56 +1100

    Routes update #2 #3

--- src/Milax/Mconsole/Commerce/Http/routes.php
+++ src/Milax/Mconsole/Commerce/Http/routes.php
@@ -1,13 +1,13 @@
 <?php
 
 Route::group([
-    'prefix' => 'mconsole',
+    'prefix' => 'mconsole/commerce',
     'middleware' => ['web', 'mconsole'],
     'namespace' => 'Milax\Mconsole\Commerce\Http\Controllers',
 ], function () {
     
-    Route::resource('commerce/categories', 'CategoriesController');
-    Route::get('commerce', 'CommerceController@index');
-    Route::resource('commerce/delivery', 'DeliveryTypesController');
+    Route::get('/', 'CommerceController@index');
+    Route::resource('categories', 'CategoriesController');
+    Route::resource('delivery', 'DeliveryTypesController');
 
 });
