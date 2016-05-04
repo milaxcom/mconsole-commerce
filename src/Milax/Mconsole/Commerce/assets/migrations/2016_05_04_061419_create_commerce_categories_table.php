1198c4b1e54edfdea805128ca3c6afd73cf57529
vladimir-r <rudenko@milax.ru>
2016-05-04 17:50:45 +1100

    Add model and migration #2

---
+++ src/Milax/Mconsole/Commerce/assets/migrations/2016_05_04_061419_create_commerce_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommerceCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commerce_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->default(0);
+            $table->integer('level')->default(0);
+            $table->string('slug')->unique();
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('commerce_categories');
+    }
+}
