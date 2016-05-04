65540922ab685ba739cf265e7ac8ba733e23d8cf
Eugene Min <e.min@milax.com>
2016-05-04 22:51:51 +1100

    EN translatons for delivery #5

---
+++ src/Milax/Mconsole/Commerce/assets/resources/lang/en/commerce.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'menu' => [
+        'index' => 'Commerce',
+        'categories' => 'Categories',
+        'delivery' => 'Delivery types',
+    ],
+    'delivery' => [
+        'tabs' => [
+            'main' => 'Main',
+        ],
+        'form' => [
+            'name' => 'Name',
+            'description' => 'Description',
+            'cost' => 'Cost',
+        ],
+    ],
+];
