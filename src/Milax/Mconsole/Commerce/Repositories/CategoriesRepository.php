e0201da9e5f91eb8ba20bbb9229683e5ee517e1a
vladimir-r <rudenko@milax.ru>
2016-05-04 17:51:05 +1100

    Add CategoriesRepository #2

---
+++ src/Milax/Mconsole/Commerce/Repositories/CategoriesRepository.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace Milax\Mconsole\Commerce\Repositories;
+
+use Milax\Mconsole\Abstractions\Repositories\EloquentRepository;
+
+class CategoriesRepository extends EloquentRepository
+{
+}
