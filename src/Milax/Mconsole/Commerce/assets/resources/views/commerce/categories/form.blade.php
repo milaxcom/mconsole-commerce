934e334588ee1082168dfaf73f33f0248d1c38b7
Eugene Min <e.min@milax.com>
2016-05-04 23:05:04 +1100

    Fix @vladimir-r categoreis form translations #2

--- src/Milax/Mconsole/Commerce/assets/resources/views/commerce/categories/form.blade.php
+++ src/Milax/Mconsole/Commerce/assets/resources/views/commerce/categories/form.blade.php
@@ -9,29 +9,29 @@
         <div class="portlet light">
             @include('mconsole::partials.portlet-title', [
                 'back' => '/mconsole/commerce/categories',
-                'title' => trans('mconsole::commerce.categiries.tabs.main'),
+                'title' => trans('mconsole::commerce.categories.tabs.main'),
                 'fullscreen' => true,
             ])
             <div class="portlet-body form">
                 
                 @if (isset($item))
                     @include('mconsole::partials.note', [
-                        'title' => trans('mconsole::commerce.categiries.info.title'),
-                        'text' => trans('mconsole::commerce.categiries.info.text'),
+                        'title' => trans('mconsole::commerce.categories.info.title'),
+                        'text' => trans('mconsole::commerce.categories.info.text'),
                     ])
                 @endif
                 
     			<div class="form-body">
     				@include('mconsole::forms.text', [
-    					'label' => trans('mconsole::commerce.categiries.form.slug'),
+    					'label' => trans('mconsole::commerce.categories.form.slug'),
     					'name' => 'slug',
     				])
     				@include('mconsole::forms.text', [
-    					'label' => trans('mconsole::commerce.categiries.form.name'),
+    					'label' => trans('mconsole::commerce.categories.form.name'),
     					'name' => 'name',
     				])
     				@include('mconsole::forms.textarea', [
-    					'label' => trans('mconsole::commerce.categiries.form.description'),
+    					'label' => trans('mconsole::commerce.categories.form.description'),
     					'name' => 'description',
     				])
     			</div>
@@ -46,7 +46,7 @@
         <div class="portlet light">
             <div class="portlet-title">
                 <div class="caption">
-                    <span class="caption-subject font-blue sbold uppercase">{{ trans('mconsole::commerce.categiries.tabs.additional') }}</span>
+                    <span class="caption-subject font-blue sbold uppercase">{{ trans('mconsole::commerce.categories.tabs.additional') }}</span>
                 </div>
             </div>
             <div class="portlet-body form">
