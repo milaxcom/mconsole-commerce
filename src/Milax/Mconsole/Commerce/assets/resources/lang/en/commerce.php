10ef63c979f2a69c23c17b1a73b8dd38a32677f0
Eugene Min <e.min@milax.com>
2016-05-06 22:34:11 +1100

    Discounts translations #13

--- src/Milax/Mconsole/Commerce/assets/resources/lang/en/commerce.php
+++ src/Milax/Mconsole/Commerce/assets/resources/lang/en/commerce.php
@@ -5,6 +5,7 @@ return [
         'index' => 'Commerce',
         'categories' => 'Categories',
         'delivery' => 'Delivery types',
+        'discounts' => 'Discounts',
     ],
     'module' => [
         'description' => 'E-shop solutions',
@@ -44,6 +45,22 @@ return [
                 'composition' => 'Composition',
                 'advantages' => 'Advantages',
             ],
+        ],
+    ],
+    'discounts' => [
+        'tabs' => [
+            'main' => 'Main',
+            'discounts' => 'Discounts table',
+        ],
+        'form' => [
+            'accumulative' => 'Accumulative',
+            'name' => 'Name',
+            'description' => 'Description',
+            'discounts' => 'Discounts table',
+            'sum' => 'Total',
+            'discount' => 'Discount value',
+            'remove' => 'Remove',
+            'append' => 'Add discount item',
         ],
     ],
 ];
