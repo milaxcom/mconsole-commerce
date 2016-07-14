175031d655bd4e25bd4ebc072351b18493a0c6e4
Eugene Min <e.min@milax.com>
2016-07-14 15:57:06 +1100

    Add CategoriesRepository contract required methods

--- src/Milax/Mconsole/Commerce/Contracts/Repositories/CategoriesRepository.php
+++ src/Milax/Mconsole/Commerce/Contracts/Repositories/CategoriesRepository.php
@@ -4,4 +4,17 @@ namespace Milax\Mconsole\Commerce\Contracts\Repositories;
 
 interface CategoriesRepository
 {
+    /**
+     * Get 'with parent' query
+     *
+     * @return mixed
+     */
+    public function withParent();
+    
+    /**
+     * Get 'with children' query
+     * 
+     * @return mixed
+     */
+    public function withChildren();
 }
