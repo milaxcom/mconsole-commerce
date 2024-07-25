db91793d1042ce09f098b6dce70e572947e85519
Vladimir Rudenko <rudenko@arkout.ru>
2024-07-26 10:23:17 +1100

    Order: fix relation with model User

--- src/Milax/Mconsole/Commerce/Models/Order.php
+++ src/Milax/Mconsole/Commerce/Models/Order.php
@@ -18,7 +18,7 @@ class Order extends Model
     
     public function user()
     {
-        return $this->belongsTo('\App\User', 'user_id');
+        return $this->belongsTo('\App\Models\User', 'user_id');
     }
     
     /**
