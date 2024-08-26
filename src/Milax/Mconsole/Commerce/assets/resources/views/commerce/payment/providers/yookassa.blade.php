b065ec70bad12aa5a316d1ef8fd1dde2d78662fc
Vladimir Rudenko <rudenko@arkout.ru>
2024-08-26 16:46:59 +1100

    Rename yandex payment provider to yookassa

--- src/Milax/Mconsole/Commerce/assets/resources/views/commerce/payment/providers/yookassa.blade.php
+++ src/Milax/Mconsole/Commerce/assets/resources/views/commerce/payment/providers/yookassa.blade.php
@@ -1,9 +1,11 @@
 @include('mconsole::forms.text', [
     'label' => trans('mconsole::commerce.payment.robokassa.shop_id'),
     'name' => 'settings[shop_id]',
+    'value' => $item->settings->shop_id ?? null,
 ])
 
 @include('mconsole::forms.text', [
     'label' => trans('mconsole::commerce.payment.robokassa.password'),
     'name' => 'settings[password]',
+    'value' => $item->settings->password ?? null,
 ])
